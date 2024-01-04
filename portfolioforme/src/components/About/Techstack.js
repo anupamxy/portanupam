b361ec42f0fce8a6d86b58e5ace2011b43d62bcd
anupamxy <123785384+anupamxy@users.noreply.github.com>
2024-01-04 15:47:23 +0530

    Techstack and tool stack
    At the bottom section of About Me page i have created a tech stack and tool stack section which are rendered as components

---
+++ portfolioforme/src/components/About/Techstack.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import { DiDjango } from "react-icons/di";
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+import { SiChakraui } from "react-icons/si";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
+  SiPostgresql,
+} from "react-icons/si";
+import { TbBrandGolang } from "react-icons/tb";
+
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaCss3Alt />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <SiChakraui />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango/>
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
